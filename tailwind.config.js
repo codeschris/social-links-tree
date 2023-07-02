67667752625f8485a1cd791f253ea3ec64fc4cc1
codeschris <chrismlolwa42@outlook.com>
2023-07-02 15:43:15 +0300

    configuring tailwind JSON

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./build/*.{html,js}"],
   theme: {
     screens: {
       sm: '480px',
